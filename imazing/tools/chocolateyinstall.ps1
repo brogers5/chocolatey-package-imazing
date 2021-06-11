2d07327c9e59995e2d3a50ee5cdc0c52d383354a
Danny J Kendall <d.manicx100@gmail.com>
2021-06-11 17:33:58 +0100

    Update imazing to 2.13.9

--- imazing/tools/chocolateyinstall.ps1
+++ imazing/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $installerType = 'exe'
 $silentArgs = '/VERYSILENT'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://downloads.imazing.com/windows/iMazing/iMazing2forWindows.exe'
-$checksum = 'C6DD637386807511068F139127881AF5EE882FD532E3E445E66BA3CAA3E8C427'
+$checksum = '47CB3FD7A9942DB6F79E75A6D80922B7CA7368216C2840B046DDE177E3F7F8B8'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
