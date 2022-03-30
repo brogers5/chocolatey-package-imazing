730d78507881710f322aa79276b62677680ad9c9
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-03-30 13:28:05 -0400

    Update to 2.14.6.0

--- imazing/tools/chocolateyinstall.ps1
+++ imazing/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-$packageName = 'imazing'
+﻿$packageName = 'imazing'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://downloads.imazing.com/windows/iMazing/iMazing2forWindows.exe'
-$checksum = ''
+$checksum = '442921a2bd651a7f83cd86ef9243481a197a0d9b1971bc4267f99c0b01b784d1'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
