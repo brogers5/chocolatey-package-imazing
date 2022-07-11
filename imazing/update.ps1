5f5e8adc3120f26f881c15472c41d5ad244d5bbf
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-07-11 02:55:54 -0400

    Overhauled auto-update implementation.
    * Query iMazing's Sparkle update service directly, to remove need to download from unversioned URL.
    * Use versioned links instead for download URL and release notes to improve long-term package reliability.

--- imazing/update.ps1
+++ imazing/update.ps1
@@ -11,25 +11,30 @@ function global:au_AfterUpdate ($Package)  {
 function global:au_SearchReplace {
     @{
         'tools\chocolateyinstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.Url32)'"
             "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+        "$($Latest.PackageName).nuspec" = @{
+            "^(\s*<releaseNotes>).*(</releaseNotes>)$" = "`$1$($Latest.ReleaseNotes)`$2"
         }
     }
 }
 
 function global:au_GetLatest {
-    $downloadUrl = "https://downloads.imazing.com/windows/iMazing/iMazing2forWindows.exe"
+    $sparkleUri = 'https://downloads.imazing.com/com.DigiDNA.iMazing2Windows.xml'
+    $userAgent = 'Update checker of Chocolatey Community Package ''imazing'''
+    $page = Invoke-WebRequest -Uri $sparkleUri -UserAgent $userAgent -UseBasicParsing
+    $xmlDocument = [xml] $page.Content
+    $latestReleaseItem = $xmlDocument.rss.channel.item[0]
 
-    $tempFile = New-TemporaryFile
-    Invoke-WebRequest -Uri $downloadUrl -OutFile $tempFile
-    $softwareVersion = $tempFile.VersionInfo.ProductVersion.Trim()
-    $checksum = (Get-FileHash -Path $tempFile -Algorithm SHA256).Hash.ToLower()
-    Remove-Item -Path $tempFile -Force
+    $marketingVersion = $latestReleaseItem.enclosure.shortVersionString
+    $productVersion = "$($marketingVersion.ToString()).0"
 
     return @{ 
-        Url32 = $downloadUrl;
-        Checksum32 = $checksum
-        Version = $softwareVersion
+        Url32 = $latestReleaseItem.enclosure.url
+        Version = $productVersion
+        ReleaseNotes = $latestReleaseItem.releaseNotesLink
     }
 }
 
-Update-Package -ChecksumFor None -NoReadme
+Update-Package -ChecksumFor 32 -NoReadme
