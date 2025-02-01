38ce0a70ed39f947b51ce91a431571aa9a2318a5
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-02-01 01:02:24 -0500

    Add /DONTSTART install argument to prevent iMazing from starting

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
     checksum       = 'd84aea2ba09a3a2fd0797b3ee93710d5bb3faa4fb21c0e10eea96b9d3ce6eb69'
     checksumType   = 'sha256'
     validExitCodes = @(0)
-    silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Install.log"
+    silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /DONTSTART /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Install.log"
 }
 
 Install-ChocolateyPackage @packageArgs
