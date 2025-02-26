5bd9fe4e6364c469e5fd41ef6f1485788e84c631
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-02-25 22:29:36 -0500

    Improve patch release detection after publishing package fix versions
    A forced package update with a 3-part version number will convert the
    version to a 4-part number. When DigiDNA creates patch releases with
    the same 3-part prefix, AU would not create a new package due to the
    patch fix version being greater.
    
    To work around this, storing a local cache of the latest consumed
    software version for each stream (which will not necessarily equate to
    the package version) when updating. When the latest package version
    happens to be a package fix version, double-check against this cache to
    determine whether we need to force an update.

--- update.ps1
+++ update.ps1
@@ -2,6 +2,8 @@
 param([switch] $Force, $IncludeStream)
 Import-Module au
 
+$lastSoftwareVersionFilePath = '.\lastSoftwareVersion.json'
+
 function Get-InstallScript($FilePath) {
     if (!(Get-Command 'innounp.exe' -ErrorAction SilentlyContinue)) {
         Write-Information 'innounp is not available on PATH, installing...'
@@ -59,6 +61,13 @@ function global:au_SearchReplace {
     }
 }
 
+function global:au_AfterUpdate($Package) {
+    #Ensure creation of package fix versions does not interfere with detection of patch releases.
+    $lastSoftwareVersions = Get-Content -Path $lastSoftwareVersionFilePath -Raw | ConvertFrom-Json -AsHashtable
+    $lastSoftwareVersions[$Latest.Stream] = $Latest.SoftwareVersion
+    $lastSoftwareVersions | ConvertTo-Json | Set-Content -Path $lastSoftwareVersionFilePath
+}
+
 function Get-ReleaseInfo($Uri) {
     $userAgent = 'Update checker of Chocolatey Community Package ''imazing'''
 
@@ -83,10 +92,41 @@ function Get-ReleaseInfo($Uri) {
     }
 }
 
+function Test-ShouldForceUpdate([System.Collections.IDictionary] $Streams) {
+    $lastSoftwareVersions = Get-Content -Path $lastSoftwareVersionFilePath -Raw | ConvertFrom-Json -AsHashtable
+
+    $result = $false
+    foreach ($stream in $lastSoftwareVersions.Keys) {
+        $lastSoftwareVersion = [version] $lastSoftwareVersions[$stream]
+
+        if ($lastPackageVersion.Major -eq $lastSoftwareVersion.Major) {
+            $currentLatestVersion = $Streams[$stream].SoftwareVersion
+            if ($lastSoftwareVersion -lt $currentLatestVersion) {
+                Write-Warning "Updated patch build detected ($currentLatestVersion), forcing package update"
+                $result = $true
+                break
+            }
+        }
+    }
+
+    return $result
+}
+
 function global:au_GetLatest {
     $streams = [ordered] @{
         V3 = Get-ReleaseInfo -Uri 'https://downloads.imazing.com/com.DigiDNA.iMazing3Windows.xml'
         V2 = Get-ReleaseInfo -Uri 'https://downloads.imazing.com/com.DigiDNA.iMazing2Windows.xml'
+    }
+
+    if (!($global:au_Force -or $Force)) {
+        [xml] $nuspec = Get-Content -Path "$($Latest.PackageName).nuspec"
+        $lastPackageVersion = [version] $nuspec.package.metadata.version
+
+        #Check whether we've previously created a package fix version.
+        #If so, we need to double-check the current patch number against a local version cache.
+        if ($lastPackageVersion.Revision -gt 9999999) {
+            $global:au_Force = Test-ShouldForceUpdate -Streams $streams
+        }
     }
 
     return @{ Streams = $streams }
