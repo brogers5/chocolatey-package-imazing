72824d43803031d65ee65df4b06af41528641532
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-03-27 18:44:03 -0400

    Update to 3.1.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ $softwareNamePattern = 'iMazing'
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'EXE'
-    url            = 'https://downloads.imazing.com/windows/iMazing/3.1.1.1/iMazing_3.1.1.1.exe'
+    url            = 'https://downloads.imazing.com/windows/iMazing/3.1.2/iMazing_3.1.2.exe'
     softwareName   = $softwareNamePattern
-    checksum       = '587f484f336491f199e0cb12595ccd5f6c98b93ee893a987f023ede0252204d5'
+    checksum       = '6c0249ce5fff7f1c8c2d6d7dcfe3618f6b0beb375e14090721030d2141f784a5'
     checksumType   = 'sha256'
     validExitCodes = @(0)
     silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /DONTINSTALLAPPLECOMPONENTS /DONTSTART /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Install.log"
